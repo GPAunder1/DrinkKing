6befa5762c1e53187fde87b046d96c5a25d9cb97
timchen7201 <timchen.nba@gmail.com>
2021-01-09 16:26:58 +0800

    Adding promotion template

--- app/presentation/assets/js/shop.js
+++ app/presentation/assets/js/shop.js
@@ -51,11 +51,16 @@ function make_toast_info(shop){
   $('#toast .opening_now').text(shop.opening_now);
   $('#toast .rating').text(shop.rating);
   $('#toast .recommend_drink').text(shop.recommend_drink);
-
+  $('#toast .promotion').html(get_shop_page(shop.name));
+  // get_shop_page(shop.name);
   make_review_info(shop.reviews);
   make_menu_info(shop.menu);
 }
-
+function get_shop_page(shop_name){
+  // var content="<div class='fb-page' data-href='https://www.facebook.com/NSYSUPhotoClub/' data-tabs='timeline' data-width='200' data-height='400' data-small-header='false' data-adapt-container-width='true' data-hide-cover='false' data-show-facepile='true'><blockquote cite='https://www.facebook.com/NSYSUPhotoClub/' class='fb-xfbml-parse-ignore'><a href='https://www.facebook.com/NSYSUPhotoClub/'>中山大學攝影社　NSYSU PHOTO CLUB</a></blockquote></div>"
+  var content="<div id='fb-root'></div> <script async='1' defer='1' crossorigin='anonymous' src='https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&amp;version=v9.0' nonce='Clfj5AXE'></script><div class='fb-page' data-href='https://www.facebook.com/NSYSUPhotoClub/' data-small-header='true' data-height='295' data-adapt-container-width='1' data-hide-cover='true' data-show-facepile='1' data-show-posts='1'><blockquote cite='https://www.facebook.com/kebuke2008/' class='fb-xfbml-parse-ignore'><a href='https://www.facebook.com/kebuke2008/'>可不可熟成紅茶</a></blockquote></div>"
+  return content
+}
 function make_review_info(reviews){
   $('#review_info').text("");
 
